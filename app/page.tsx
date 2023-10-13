4026a0005e17d417b3b8dac27c6b1029af50cd81
prem0556 <kumarp73616@gmail.com>
2023-10-13 09:34:13 +0530

    added test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="main">
-      <Button/>
+      <Button variant={"outline"} size={"lg"}>
+        Click me
+      </Button>
     </div>
   )
 }
