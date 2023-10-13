b4de3d7eadcb476370914f067075c44dc19861a8
prem0556 <kumarp73616@gmail.com>
2023-10-13 11:52:53 +0530

    added footer

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "./_component/footer";
 import { Heading } from "./_component/heading";
 import { Heroes } from "./_component/heroes";
 
@@ -8,6 +9,7 @@ const LandingPage = () => {
     <div className="flex flex-col items-center justify-center md:justify-center text-center gap-y-8 flex-1 px-6 pb-10">
       <Heading/>
       <Heroes/>
+      <Footer/>
     </div>
   </div>);
 }
