6c487b2c58b69e46663362fb48f5363c0ed61003
prem0556 <kumarp73616@gmail.com>
2023-10-13 11:41:17 +0530

    aaded landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,15 @@
+import { Heading } from "./_component/heading";
+import { Heroes } from "./_component/heroes";
+
+
+const LandingPage = () => {
+  return (
+  <div className=" min-h-full flex flex-col">
+    <div className="flex flex-col items-center justify-center md:justify-center text-center gap-y-8 flex-1 px-6 pb-10">
+      <Heading/>
+      <Heroes/>
+    </div>
+  </div>);
+}
+
+export default LandingPage;
