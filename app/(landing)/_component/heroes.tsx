24c855bd61db79d1905353c30221c91a85f47726
prem0556 <kumarp73616@gmail.com>
2023-10-13 11:41:34 +0530

    added more

---
+++ app/(landing)/_component/heroes.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export const Heroes=() =>{
+    return(
+        <div className="flex flex-col items-center justify-start max-w-5xl">
+            <div className="flex items-center">
+                <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+                    <Image 
+                    src={"/back.png"}
+                    fill
+                    alt="brain"
+                    className=" object-contain"
+                    />
+                </div>
+            
+            </div>
+            
+        </div>
+    )
+}
