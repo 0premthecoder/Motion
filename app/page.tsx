d9c915a38dd525f7ea00b41a82e2969360d8b4ab
prem0556 <kumarp73616@gmail.com>
2023-10-13 09:13:52 +0530

    added btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-red-500 font-mono'>Hello World</p>
+    <div className="main">
+      <Button/>
+    </div>
   )
 }
  
