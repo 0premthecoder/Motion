60946acc8d5a3741876a87cbdbf4d5c34306131d
prem0556 <kumarp73616@gmail.com>
2023-10-13 12:02:46 +0530

    added footer

--- app/(landing)/_component/footer.tsx
+++ app/(landing)/_component/footer.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { Logo } from "./logo";
 
 const Footer = () => {
     return (
         <div className="flex items-center w-full p-6 bg-background z-50">
             <Logo/>
+            <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+                <Button variant={"ghost"}>Terms & Conditions</Button>
+                <Button variant={"ghost"}>Privacy Policy</Button>
+
+            </div>
         </div>
     );
 }
--- app/(landing)/_component/logo.tsx
+++ app/(landing)/_component/logo.tsx
@@ -4,17 +4,19 @@ import { cn } from "@/lib/utils";
 
 const font = Puppies_Play(
     {
-        subsets:["vietnamese"],
-        weight: ["400","400"]
+        subsets: ["vietnamese"],
+        weight: ["400", "400"]
     }
 )
 
-export const Logo =()=>{
-    return(<div className="hidden md:flex items-center gap-x-2">
-        <Image 
-        src={"/dark/favicon.ico"}
-        width={"20"}
-        height={"20"}
-        alt="Logo"/>
+export const Logo = () => {
+    return (<div className="hidden md:flex items-center gap-x-2">
+        <Image
+            src={"/dark/favicon.ico"}
+            width={"20"}
+            height={"20"}
+            alt="Logo" />
+
+        <p className={cn(" font-semibold", font.className)}> Motion</p>
     </div>)
 }
