acb7ecae87684b6867197de9750ca8c255c9e873
prem0556 <kumarp73616@gmail.com>
2023-10-13 17:13:09 +0530

    now we can toggle modes

--- app/(landing)/_component/navbar.tsx
+++ app/(landing)/_component/navbar.tsx
@@ -3,13 +3,15 @@
 import { UseScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const Navbar = () => {
 
     const scrolled = UseScrollTop()
-    return ( <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6", scrolled && " border-b shadow-sm")}>
+    return ( <div className={cn("z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && " border-b shadow-sm")}>
         <Logo/>
-        <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2"> Login </div>
+        <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2"><ModeToggle/> </div>
+        
     </div> );
 }
  
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import { Heroes } from "./_component/heroes";
 
 const LandingPage = () => {
   return (
-  <div className=" min-h-full flex flex-col">
+  <div className=" min-h-full flex flex-col dark:bg-[#1F1F1F]">
     <div className="flex flex-col items-center justify-center md:justify-center text-center gap-y-8 flex-1 px-6 pb-10">
       <Heading/>
       <Heroes/>
