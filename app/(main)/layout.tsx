4f6af8746939e3b1aed4e2feae1b0fe8446c8ce9
prem0556 <kumarp73616@gmail.com>
2023-10-14 08:13:16 +0530

    added document page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,26 @@
 "use client"
 
+import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
 
 const MainLayout = ({children}:{
     children: React.ReactNode
 }) => {
     const {isLoading, isAuthenticated} = useConvexAuth()
-    return ( <>{children}</> );
+
+    if (isLoading){
+        return (<div className="h-full flex items-center justify-center">
+        <Spinner size="lg"/></div>
+        )
+    }
+
+    if (!isAuthenticated){
+        return redirect("/")
+    }
+    return ( <div className="h-full flex dark:bg-[#1f1f1f}">
+        {children}
+        </div> );
 }
  
 export default MainLayout;
