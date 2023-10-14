f62a5dcaa6ee6b1092f5bba1dff1efa7c24356dc
prem0556 <kumarp73616@gmail.com>
2023-10-14 12:47:18 +0530

    created document page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,38 @@
+"use client"
+
+import Image from "next/image";
+import { SignOutButton, UserButton, useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusSquareIcon } from "lucide-react";
+
 const DocumentsPage = () => {
-    return ( <div>This is a protected page</div> );
+
+    const { user } = useUser()
+
+    return ( <div className="h-full flex flex-col items-center justify-center space-y-4">
+        <Image 
+            src={"/write-light.png"}
+            height={300}
+            width={300}
+            alt="brain"
+            className="dark:hidden"
+        />
+        <Image 
+            src={"/write-dark.png"}
+            height={300}
+            width={300}
+            alt="brain"
+            className="hidden dark:block"
+            />
+        <h2 className="text-lg font-medium">
+            Welcome to {user?.firstName}&apos; Motion
+        </h2>
+        <Button>
+            Create New Note
+            <PlusSquareIcon className="h-4 w-4 ml-2"/>
+        </Button>
+
+    </div> );
 }
  
 export default DocumentsPage;
