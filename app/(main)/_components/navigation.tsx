9ad47d8a1f2c907bdf595509d5f57c9219cf274b
prem0556 <kumarp73616@gmail.com>
2023-10-14 08:32:56 +0530

    added button for resize

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { ChevronLeftIcon } from "lucide-react";
+
+const Navigation = () => {
+    return (<>
+        <aside className="group/sidebar h-full overflow-y-auto relative flex w-60 flex-col bg-secondary z-[99999]"
+        >
+            <div role="button" className=" h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition">
+                <ChevronLeftIcon className="h-6 w-6"/>
+            </div>
+            <div>
+                <p>Actoin Items</p>
+            </div>
+            <div className=" mt-4">
+                <p>Documets</p>
+            </div>
+            <div className=" opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute w-1 h-full bg-primary/10 right-0 top-0"/>
+        </aside>
+    </>);
+}
+
+export default Navigation;
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,24 +3,29 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
 
-const MainLayout = ({children}:{
+const MainLayout = ({ children }: {
     children: React.ReactNode
 }) => {
-    const {isLoading, isAuthenticated} = useConvexAuth()
+    const { isLoading, isAuthenticated } = useConvexAuth()
 
-    if (isLoading){
+    if (isLoading) {
         return (<div className="h-full flex items-center justify-center">
-        <Spinner size="lg"/></div>
+            <Spinner size="lg" /></div>
         )
     }
 
-    if (!isAuthenticated){
+    if (!isAuthenticated) {
         return redirect("/")
     }
-    return ( <div className="h-full flex dark:bg-[#1f1f1f}">
-        {children}
-        </div> );
+    return (<div className="h-full flex dark:bg-[#1f1f1f}">
+        <Navigation/>
+        <main className="flex-1 h-full overflow-y-auto">
+
+            {children}
+        </main>
+    </div>);
 }
- 
+
 export default MainLayout;
