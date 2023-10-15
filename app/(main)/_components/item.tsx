44c3bc3e011c9e586764825aa5c95d23b11c35f4
prem0556 <kumarp73616@gmail.com>
2023-10-15 08:58:11 +0530

    added some properties to item

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,16 +1,31 @@
 "use client"
 
+import { Id } from "@/convex/_generated/dataModel"
 import { LucideIcon } from "lucide-react"
 
-interface ItemProps{
+interface ItemProps {
+    id?: Id<"documents">
+    documentIcon?: string
+    active?: boolean
+    expanded?: boolean
+    isSearch?: boolean
+    level?: number
+    onExpand?: () => void
+
     label: string
-    onClick: ()=> void
+    onClick: () => void
     icon: LucideIcon
 }
 
-const Item = ({label, onClick, icon:Icon}:ItemProps) => {
-    return (<div onClick={onClick} role="button" style={{paddingLeft: "12px"}} className="group min-h-[27px] text-sm py-1 pr-1 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"><Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground"/>
-      <span>  {label} </span>
+const Item = ({ id,
+    documentIcon,
+    active,
+    expanded,
+    isSearch,
+    level,
+    onExpand, label, onClick, icon: Icon }: ItemProps) => {
+    return (<div onClick={onClick} role="button" style={{ paddingLeft: "12px" }} className="group min-h-[27px] text-sm py-1 pr-1 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"><Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+        <span>  {label} </span>
 
     </div>);
 }
