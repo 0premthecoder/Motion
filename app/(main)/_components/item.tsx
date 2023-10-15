e8ce2f079c3efd3266570355f0d32f2031d94bd2
prem0556 <kumarp73616@gmail.com>
2023-10-15 09:22:05 +0530

    item somehow completed

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -57,6 +57,14 @@ const Item = ({ id,
             }
             <span>  {label} </span>
 
+            {isSearch && (
+                <kbd className=" ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+                    <span className=" text-xs ">
+                    &#8984;
+                    </span>k
+                </kbd>
+            )}
+
         </div>);
 }
 
