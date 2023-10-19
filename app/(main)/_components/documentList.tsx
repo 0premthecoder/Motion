25623311c0c0cb0d53e59898539ff041a6021941
prem0556 <kumarp73616@gmail.com>
2023-10-19 10:08:57 +0530

    fixed bug

--- app/(main)/_components/documentList.tsx
+++ app/(main)/_components/documentList.tsx
@@ -3,16 +3,16 @@
 import { api } from "@/convex/_generated/api";
 import { Doc, Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
-import { redirect, useParams, useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import Item from "./item";
 import { cn } from "@/lib/utils";
 import { FileIcon } from "lucide-react";
 
-interface DocumentListProps{
+interface DocumentListProps {
     parentDocumentId?: Id<"documents">
-    level?:number
-    data?:Doc<"documents">[]
+    level?: number
+    data?: Doc<"documents">[]
 
 }
 
@@ -23,54 +23,54 @@ const DocumentList = ({
 }: DocumentListProps) => {
     const params = useParams()
     const router = useRouter()
-    const [expand, setExpand] = useState<Record<string,boolean>>({})
+    const [expand, setExpand] = useState<Record<string, boolean>>({})
 
-    const onExpand = (documentId: string) =>{
-        setExpand(prevExpanded=>({
+    const onExpand = (documentId: string) => {
+        setExpand(prevExpanded => ({
             ...prevExpanded,
-            [documentId]:  !prevExpanded[documentId]
+            [documentId]: !prevExpanded[documentId]
 
         }))
     }
 
-    const documents = useQuery(api.document.getSidebar,{
+    const documents = useQuery(api.document.getSidebar, {
         parenDocument: parentDocumentId
     })
 
-    const onReDirect = (documentId: string)=>{
+    const onReDirect = (documentId: string) => {
         router.push(`/documests/${documentId}`)
     }
 
-    if(documents === undefined){
-        return(<>
-        <Item.Skeleton level={level}/>
-        {level === 0 && <>
-            <Item.Skeleton level={level}/>
-            <Item.Skeleton level={level}/>
-        </>}
+    if (documents === undefined) {
+        return (<>
+            <Item.Skeleton level={level} />
+            {level === 0 && <>
+                <Item.Skeleton level={level} />
+                <Item.Skeleton level={level} />
+            </>}
         </>)
     }
 
-    return ( <>
+    return (<>
         <p
             style={{
-                paddingLeft: level ? `${(level * 12)+25}px`: undefined
+                paddingLeft: level ? `${(level * 12) + 25}px` : undefined
             }}
             className={cn(
-                "hidden text-sm font-medium to-muted-foreground/80", expand &&" last:block", level ===0 && "hidden"
+                "hidden text-sm font-medium text-muted-foreground/80", expand && " last:block", level === 0 && "hidden"
             )}
         >No pages Inside</p>
-        {documents.map((document)=>{
+        {documents.map((document) => (
             <div key={document._id}>
-                <Item id={document._id} onClick={()=> redirect(document._id)} label={document.title} icon={FileIcon} documentIcon={document.icon} active={params.documentId === document._id } level={level} onExpand={()=> onExpand(document._id)} 
-                expanded={expand[document._id]}
+                <Item id={document._id} onClick={() => onReDirect(document._id)} label={document.title} icon={FileIcon} documentIcon={document.icon} active={params.documentId === document._id} level={level} onExpand={() => onExpand(document._id)}
+                    expanded={expand[document._id]}
                 />
                 {expand[document._id] && (
-                    <DocumentList parentDocumentId={document._id} level={level + 1}/>
-        )}
+                    <DocumentList parentDocumentId={document._id} level={level + 1} />
+                )}
             </div>
-        })}
-    </> );
+        ))}
+    </>);
 }
- 
+
 export default DocumentList;
