075769b60667df2afdf388892aa1f925d85f71a6
prem0556 <kumarp73616@gmail.com>
2023-10-20 17:55:58 +0530

    adding modal

---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { AlertDialog, 
+    AlertDialogAction, 
+    AlertDialogCancel, 
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger
+    
+     } from "@/components/ui/alert-dialog"
+
+interface ConfirmModalProps{
+    children: React.ReactNode;
+    onConfirm:()=> void
+}
