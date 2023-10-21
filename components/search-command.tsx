8fdf580ae7a4b4bd359873e5685a8f87246a27eb
prem0556 <kumarp73616@gmail.com>
2023-10-21 11:22:36 +0530

    creating search command

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -18,5 +18,29 @@ import { useSearch } from '@/hooks/use-search'
 import { api } from '@/convex/_generated/api'
 
 export const SearchCommand = ()=>{
+    const { user} = useUser();
+    const router = useRouter();
+    const documents = useQuery(api.document.getSearch);
+    const [isMounted, setIsMounted] = useState(false)
+
+    const toggle = useSearch((store) => store.toggle)
+    const isOpen = useSearch((store) => store.isOpen)
+    const onClose = useSearch((store) => store.onClose)
+
+    useEffect(() => {
+      setIsMounted(true)
+    }, [])
+
+    if(!isMounted){
+        return null
+    }
+
+    return (
+        <CommandDialog open={isOpen} onOpenChange={onClose}>
+            <CommandInput placeholder={`Search${user?.fullName}'s Motion...`}/>
+
+        </CommandDialog>
+    )
     
+
 }
