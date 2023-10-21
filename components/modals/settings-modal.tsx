5178653b981ba448fbc21896d582342a1c698ed9
prem0556 <kumarp73616@gmail.com>
2023-10-21 14:46:06 +0530

    created settings-modal

---
+++ components/modals/settings-modal.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Dialog,  
+     DialogContent,
+     DialogDescription,
+     DialogFooter,
+     DialogHeader,
+     DialogTitle,
+     DialogTrigger
+    
+     } from "@/components/ui/dialog"
+
+import { useSettings } from "@/hooks/use-settings"
+import { Label } from "../ui/label"
+import { ModeToggle } from "../mode-toggle"
+
+export const SettingsModal = ()=>{
+    const settings = useSettings()
+
+    return (
+        <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
+            <DialogContent>
+                <DialogHeader className="border-b pb-3">
+                    <h2 className="text-lg font-medium">My Setting</h2>
+                </DialogHeader>
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col gap-y-1">
+                        <Label>Appereance</Label>
+                        <span className="text-[0.8rem] text-muted-foreground">
+                            Customize Your Jotion Looks
+                        </span>
+                    </div>
+                    <ModeToggle/>
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
