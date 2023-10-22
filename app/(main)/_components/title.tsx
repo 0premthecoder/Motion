206a30333cafa8e5ed619649c7daa8d6ed3fc892
prem0556 <kumarp73616@gmail.com>
2023-10-22 09:09:51 +0530

    added title

---
+++ app/(main)/_components/title.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface TitleProps {
+    initialData: Doc<"documents">
+
+}
+
+const Title = ({ initialData }: TitleProps) => {
+    return (<>Title</>);
+}
+
+export default Title;
