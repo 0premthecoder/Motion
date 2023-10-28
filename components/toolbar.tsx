a4e42a3201bf6589a2fa0b4b17949da08a6081df
prem0556 <kumarp73616@gmail.com>
2023-10-28 11:09:25 +0530

    stay hard

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,8 +8,11 @@ interface toolbarProps {
 
 }
 
-const Toolbar = ({initialData, preview}:toolbarProps) => {
-    return ( <>Yd</> );
+const Toolbar = ({ initialData, preview }: toolbarProps) => {
+    return (<div className="pl-[54px] gropu relative"> {!!initialData && !preview && 
+    <div className="flex items-center gap-x-2 group/icon pt-6">
+        
+    </div>}</div>);
 }
- 
+
 export default Toolbar;
