dd78052d75888c46e8ffc2024b64a7ce58df95b1
prem0556 <kumarp73616@gmail.com>
2023-10-28 12:04:03 +0530

    icon picker created

---
+++ components/icon-picker.tsx
@@ -0,0 +1,27 @@
+"use client"
+import EmojiPicker, {Theme} from "emoji-picker-react"
+import { useTheme } from "next-themes"
+import { Popover, 
+    PopoverTrigger,
+    PopoverContent,
+ } from "@/components/ui/popover";
+
+
+interface IconPickerProps {
+    onChange: (icon: string)=>void,
+    children: React.ReactNode,
+    asChild?: boolean
+}
+
+export const IconPicker = ({onChange,children,asChild,}: IconPickerProps) =>{
+    const {resolvedTheme} = useTheme()
+    const currentTheme = (resolvedTheme || "light") as keyof typeof themeMap
+
+    const themeMap  = {
+        "dark":Theme.DARK,
+        "light":Theme.LIGHT
+    }
+
+    const theme = themeMap[currentTheme]
+
+}
