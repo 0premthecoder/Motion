04783b98883af21502776a52e0f8301a47baca61
prem0556 <kumarp73616@gmail.com>
2023-10-30 14:31:01 +0530

    design fixed

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -112,7 +112,7 @@ const Item = ({ id,
                     {documentIcon}
                 </div>
             ) : (
-                <><Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" /></>)
+                <><Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" /></>)
             }
             <span>  {label} </span>
 
