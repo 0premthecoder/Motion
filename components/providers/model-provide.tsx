90c40b9432f6daff6a85429e3ed46b956ccc4502
prem0556 <kumarp73616@gmail.com>
2023-10-30 08:27:54 +0530

    imported cover image modal in modal provider

--- components/providers/model-provide.tsx
+++ components/providers/model-provide.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react"
 import { SettingsModal } from "../modals/settings-modal"
+import { CoverImageModal } from "../modals/cover-image-modal"
 
 export const ModelProvider = ()=>{
     
@@ -16,6 +17,7 @@ export const ModelProvider = ()=>{
     return (
         <>
             <SettingsModal/>
+            <CoverImageModal/>
         </>
     )
 }
